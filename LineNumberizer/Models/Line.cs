ccb0f713b867d2d4b7625262c5bd0b6e2ff422a0
Akn <sibata486@gmail.com>
2024-08-12 23:02:52 +0900

    add / 二目のプレフィックスプロパティを追加。

--- LineNumberizer/Models/Line.cs
+++ LineNumberizer/Models/Line.cs
@@ -8,10 +8,13 @@ namespace LineNumberizer.Models
         private string prefix = string.Empty;
         private int prefixNumber;
         private string lineNumber = string.Empty;
+        private string secondPrefix;
 
         public string Text { get => text; set => SetProperty(ref text, value); }
 
         public string Prefix { get => prefix; set => SetProperty(ref prefix, value); }
+
+        public string SecondPrefix { get => secondPrefix; set => SetProperty(ref secondPrefix, value); }
 
         public int PrefixNumber { get => prefixNumber; set => SetProperty(ref prefixNumber, value); }
 
